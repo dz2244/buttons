d18cda6e80f106665682a746aa95d71982b4ad4f
dz2244 <dz2244@bs.amalnet.k12.il>
2023-12-12 21:48:55 +0200

    commit and push

--- app/src/main/java/com/example/buttons/MainActivity.java
+++ app/src/main/java/com/example/buttons/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.buttons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    public void click(View view) {
+        btn = findViewById(R.id.btn);
+        btn.setText("Oh, yea, I’ve been clicked!");
+    }}
